0a32d9912327e8dd3d3fea095d41be1760e01fd6
Pascal Ulor <pascalulor@yahoo.com>
2022-01-23 00:41:06 +0100

    add heroku url to client

--- server/api/server.ts
+++ server/api/server.ts
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production') {
   // TODO serve client
   app.use(express.static(path.resolve(__dirname, '../../../client/build')));
   app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, '../../../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../..', 'client', 'build', 'index.html'));
   });
 }
 
